4677e75440dee68e60b2b2fea76a38d79beafe0d
Marlon Malagon <maincoke@gmail.com>
2019-03-30 14:36:32 -0500

    Depuración de código - Configuración de SnackBar para notificar mensajes

--- server/models.js
+++ server/models.js
@@ -1,7 +1,7 @@
 /**
  * Configuración y exportación del Esquema de Datos para BD de MongoDB
  *
- * Importacion de paquetes instalados para configurar los esquemas de datos de Nivel 1 y 2
+ * Importacion de paquetes instalados para configurar los esquemas de datos de Nivel 1, 2 y 3
  */
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -21,16 +21,21 @@ module.exports = ProductData;
 /**
  * ** ///////////// ** ////////////// ** ///////////// ** ////////////// ** ///////////// ** //////////////
  */
-/* Esquema de Datos de los Productos del Carrito de Usuario - Tienda Online - Nivel 2 */
-let ShopCarSchema = new dataSchema({
-  order: { type: Number, unique: true, require: true, alias: 'scodr'},
+/* Esquema de Datos de los Productos del Carrito de Usuario - Tienda Online - Nivel 3 */
+let ShopCarProdsSchema = new dataSchema({
   id: { type: objectId, alias: 'scpid' },
   price: { type: Date, alias: 'scpce' },
-  quantt: { type: Date, alias: 'scstk' },
-  paidod: { type: Boolean, require: true, alias: 'scpdo' }
+  quantt: { type: Date, alias: 'scstk' }
 }, { id: false, _id: false });
 
-/* Esquema de Datos del Usuario - Nivel 1 */
+/* Esquema de Datos del Carrito de Usuario - Tienda Online - Nivel 2 */
+let ShopCarSchema = new dataSchema({
+  order: { type: Number, unique: true, require: true, alias: 'scodr'},
+  paidod: { type: Boolean, require: true, alias: 'scpdo' },
+  products: [ShopCarProdsSchema]
+}, { id: false, _id: false });
+
+/* Esquema de Datos del Usuario - Tienda Online - Nivel 1 */
 let UserSchema = new dataSchema({
   nameuser: { type: String, require: true, alias: 'usnme' },
   emailusr: { type: String, lowercase: true, trim: true, require: true, alias: 'useml' },
